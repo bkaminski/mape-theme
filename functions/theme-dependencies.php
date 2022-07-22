852ce9f87df753d9610a6826be569fb573efd887
Ben Kaminski <ben@benkaminski.com>
2022-07-21 22:41:00 -0400

    brought hero text to top of carousel

--- functions/theme-dependencies.php
+++ functions/theme-dependencies.php
@@ -2,7 +2,7 @@
 //LOAD SCRIPTS
 function enqueue_bkc_scripts() {
     wp_enqueue_script('Bootstrap-5.2', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', array('jquery'), '', true);
-    wp_enqueue_script('font-awesome-5', 'https://kit.fontawesome.com/42219cbebc.js', false, null, null, false);
+    wp_enqueue_script('font-awesome-6', 'https://kit.fontawesome.com/dd00380525.js', false, null, null, false);
     wp_enqueue_script('bkc-scripts', get_template_directory_uri() . '/assets/js/bkcScripts.min.js', array('jquery'), null, true, null);
 }
 add_action('wp_enqueue_scripts', 'enqueue_bkc_scripts');
--- parts/home-hero.php
+++ parts/home-hero.php
@@ -1,29 +1,19 @@
 <section class="mape-hero">
 	<div id="carousel1" class="carousel slide carousel-fade" data-bs-ride="carousel">
+		<div class="carousel-caption" style="z-index: 9;">
+			<h1 class="fw-bold hero--title">Mid-Atlantic Propane Exchange</h1>
+			<p class="h3 pb-3 hero--sub">Clean, Efficient Energy </p>
+			<p><a href="#" class="btn btn-lg btn-warning text-uppercase fw-bold hero--cta">Find out More</a></p>
+		</div>
 		<div class="carousel-inner">
 			<div class="carousel-item active">
 				<img src="<?php echo get_home_url();?>/wp-content/uploads/mape-hero-image.jpg" class="d-block w-100" alt="...">
-				<div class="carousel-caption">
-					<h1 class="fw-bold hero--title">Mid-Atlantic Propane Exchange</h1>
-					<p class="h3 pb-3 hero--sub">Clean, Efficient Energy </p>
-					<p><a href="#" class="btn btn-lg btn-warning text-uppercase fw-bold hero--cta">Find out More</a></p>
-				</div>
 			</div>
 			<div class="carousel-item">
 				<img src="<?php echo get_home_url();?>/wp-content/uploads/mape-hero-image2.jpg" class="d-block w-100" alt="...">
-				<div class="carousel-caption">
-					<h1 class="fw-bold hero--title">Mid-Atlantic Propane Exchange</h1>
-					<p class="h3 pb-3 hero--sub">Clean, Efficient Energy </p>
-					<p><a href="#" class="btn btn-lg btn-warning text-uppercase fw-bold hero--cta">Find out More</a></p>
-				</div>
 			</div>
 			<div class="carousel-item">
 				<img src="<?php echo get_home_url();?>/wp-content/uploads/mape-hero-image3.jpg" class="d-block w-100" alt="...">
-				<div class="carousel-caption">
-					<h1 class="fw-bold hero--title">Mid-Atlantic Propane Exchange</h1>
-					<p class="h3 pb-3 hero--sub">Clean, Efficient Energy </p>
-					<p><a href="#" class="btn btn-lg btn-warning text-uppercase fw-bold hero--cta">Find out More</a></p>
-				</div>
 			</div>
 		</div>
 	</div>
