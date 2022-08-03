4b65d9084bdc55f4795282757998fe9a40a0e9a4
Ben Kaminski <ben@benkaminski.com>
2022-08-02 21:36:41 -0400

    fixed contact page overflow

---
+++ parts/page-int-page-header.php
@@ -0,0 +1,4 @@
+<section class="int-page-header pt-5 pb-5">
+    <?php the_title( '<h1 class="text-center text-white int--page--title">', '</h1>' ); ?>
+        
+</section>
---
+++ parts/page-tag-container.php
@@ -0,0 +1,8 @@
+<div class="tag-container">
+    <div class="container">
+        <div class="col pb-3 pt-3">
+            <?php the_tags('<span class="badge bg-dark"><i class="fa-solid fa-tags"></i>&nbsp;Related Topics:</span>&nbsp;&nbsp;'); ?>
+                
+        </div>
+    </div>
+</div>
